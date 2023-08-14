f7abde761896ca3e0b3b106ef393a15e2f1bf225
Kaique Bosco <kaique.bosco@hotmail.com>
2023-08-14 12:51:00 -0300

    event section content + responsivity

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -12,14 +12,14 @@ AOS.init({
     // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
     offset: 120, // offset (in px) from the original trigger point
     delay: 0, // values from 0 to 3000, with step 50ms
-    duration: 1000, // values from 0 to 3000, with step 50ms
+    duration: 1500, // values from 0 to 3000, with step 50ms
     easing: "ease", // default easing for AOS animations
     once: false, // whether animation should happen only once - while scrolling down
     mirror: false, // whether elements should animate out while scrolling past them
     anchorPlacement: "top-bottom", // defines which position of the element regarding to window should trigger the animation
 });
 
-const eventDate = new Date("Jun 19, 2024 16:00:00");
+const eventDate = new Date("Jun 19, 2024 08:00:00");
 const eventTimeStamp = eventDate.getTime();
 
 const dateCount = setInterval(function () {
@@ -48,6 +48,7 @@ const dateCount = setInterval(function () {
 
     if (eventDistance < 0) {
         clearInterval(dateCount);
-        document.getElementById("hero__text").innerHTML = "O evento já começou! Fique por dentro para os próximos.";
+        document.getElementById("hero__text").innerHTML =
+            "O aniversário deste ano já passou, mas ano que vêm tem mais! Nos vemos na próxima";
     }
 }, 1000);
